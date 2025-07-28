d898225f91c4fd45cb5b10f12b6a846659907965
warud <muhammadsalwarud@gmail.com>
2025-07-28 23:55:19 +0700

    feat: Implement POST /transactions/ endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,24 @@
-from fastapi import FastAPI
-import models
-from database import engine
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+import crud, models, schemas
+from database import SessionLocal, engine
 
-# Baris ini memerintahkan SQLAlchemy untuk membuat semua tabel dari models
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# Dependency untuk mendapatkan sesi database
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/transactions/", response_model=schemas.Transaction)
+def create_transaction(transaction: schemas.TransactionCreate, db: Session = Depends(get_db)):
+    return crud.create_transaction(db=db, transaction=transaction)
+
 @app.get("/")
 def root():
-    return {"message": "Selamat datang di AccounTech API"}
+    return {"message": "AKHIRNYAAA BISAAAA!"}
