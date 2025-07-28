dd666a021e1f7ead842e9129ee76d4f565a48c3f
warud <muhammadsalwarud@gmail.com>
2025-07-28 23:27:05 +0700

    feat: setup the table in database

---
+++ backend/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from .database import Base
+import datetime
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    date = Column(DateTime, default=datetime.datetime.now)
+    description = Column(String(255), index=True)
+    type = Column(String(50))  # Misal: "Penjualan", "Pembelian", "Biaya Operasional"
+    amount = Column(Float)
